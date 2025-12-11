7606c89cf6db7f4e0406f3a7963613ed542f7d98
ackerm0n <qaq520131466@icloud.com>
2025-12-11 20:47:50 +0800

    Fix vLLM version configuration - set to 0.11.0 to match original release

---
+++ vllm/_version.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+"""Version information for vLLM."""
+
+__version__ = "0.11.0"
+__version_tuple__ = (0, 11, 0)
